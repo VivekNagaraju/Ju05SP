d28509a616813242ec45a8f3ef484470759c8d31
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2025-07-10 08:47:10 +0530

    10-07-25: Control flow statements

---
+++ PythonTutorials/control_flow_statements/__init__.py
@@ -0,0 +1,13 @@
+'''
+Control flow statements:
+
+Statements which control the flow of execution of a program
+
+Types of controls:
+1. Sequential flow: All statements are executed one after another
+2. Conditional flow: Certain set of statements are executed if a condition is satisfied (based on a condition)
+    - If condition gives you True as result one set of statements will be executed
+    - If condition gives you False as result another set of statements will be executed
+    - Conditional statements will executed only once per execution
+3. Looping: A set of statements will be executed repeatedly until a condition is fulfilled
+'''
