6dcf133e982281b093bbae077da6da5fb144af52
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2025-07-14 09:20:23 +0530

    14-07-25: Loop control statements

--- PythonTutorials/control_flow_statements/vi_looping_statements.py
+++ PythonTutorials/control_flow_statements/vi_looping_statements.py
@@ -11,6 +11,10 @@ Types of looping statements:
     c. Increment/ decrement
     
 2. For loop: used to iterate over a collection/ range
+
+Loop control statements/ keywords:
+1. Break: it breaks the execution of a loop upon some condition is met 
+2. Continue: skips the current execution of a loop based on a condition
 '''
 '''
 count=0
@@ -25,8 +29,31 @@ count=10
 while count>0:
     print(count)
     count-=1
+    if count == 5:
+        break
 '''
-print(range(10))
 
-for i in range(1, 100):
+count=10
+while count>0:
+    
+    if count == 5:
+        count-=1
+        continue
+    
+    print(count)
+    count-=1
+    
+
+# print(range(10))
+'''
+for i in range(2, 100, 2):
     print(i)
+    if i == 50:
+        break
+'''
+'''
+for i in range(2, 100, 2):
+    if i == 50:
+        continue
+    print(i)
+'''
