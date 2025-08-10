59c49860bb3c5af0ae0dcc80428e7b96b5d74d51
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2025-08-10 15:32:28 +0530

    10-08-25

---
+++ PythonTutorials/oops/__init__.py
@@ -0,0 +1,30 @@
+'''
+OOPS: Object-Oriented Programming System or Object-Oriented Programming Structure.
+
+OOPS is style/design/paradigm of writing a program based on an object
+
+Objects: real world entities which have features and perform some functions(behaviors)
+
+features --> variables
+behaviors --> functions
+
+We need an entity to bind these variables and functions together, but we can't have object as basic entity.
+
+Because:
+1. There can be multiple objects of same type, Ex: Blue zombies: 
+    When we define objects we have to repeat the code for variables and functions every time we create a Blue zombie.
+2. Objects are independent of each other: Changes made on an object should not affect others.
+    Ex: When I hit a blue zombie that one should die and others should not be affected
+    
+If we keenly observe any object is a member of larger type/ category/ class.
+
+We decided to first define a class consisting of features and functions.
+
+Then using this class as blueprint/ mould to create distinct/separate objects of that class.
+
+
+
+
+    
+    
+'''
