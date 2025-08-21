ee56c31eab1feb4f1fc03004daca497f9ac16d47
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2025-08-21 10:19:33 +0530

    21-08-25

--- ViSeleniumTutorials/vi_selenium_basics/vi_launch_browser.py
+++ ViSeleniumTutorials/vi_selenium_basics/vi_launch_browser.py
@@ -19,7 +19,7 @@ driver.get("https://testautomationpractice.blogspot.com/2018/09/automation-form.
 
 # 3. Enter name
 # Locating
-name_txt_bx = driver.find_element(By.ID, 'name')
+name_txt_bx = driver.find_element(By.CSS_SELECTOR, '#name')
 
 # Action
 name_txt_bx.send_keys("Vivek")
@@ -29,3 +29,12 @@ name_txt_bx.send_keys("Vivek")
 male_radio_button = driver.find_element(By.ID, "male")
 #Action
 male_radio_button.click()
+
+'''
+"/html/body/div[4]/div[2]/div[2]/div[2]/div[2]/div[2]/div[2]/div/div[4]/div[1]/div/div/div[1]/div[1]/div/div/div/div/div[2]/div[1]/input[1]"
+'//*[@id="name"]'
+
+'//*[@id="HTML4"]/div[1]/button'
+'//button[@onclick="myFunction()"]'
+'//div[@data="//button[normalize-space()='Checkout here'][@class='dropbtn']"]'
+'''
